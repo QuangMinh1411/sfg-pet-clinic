402a12c6e8c0597c9f032b884b81f781c5a429ad
QuangMinh1411 <TomMinhQuan1411@gmail.com>
2022-07-06 11:25:29 +0700

    Create Interfaces for Pet Clinic Services close #5

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/heaven/sfgpetclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package com.heaven.sfgpetclinic.services;
+
+import com.heaven.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
