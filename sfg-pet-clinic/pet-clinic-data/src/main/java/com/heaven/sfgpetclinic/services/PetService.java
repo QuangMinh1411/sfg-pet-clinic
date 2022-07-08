6768404356c729f64673fff68e92244157032db2
QuangMinh1411 <TomMinhQuan1411@gmail.com>
2022-07-08 15:12:01 +0700

    Refactor Service

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/heaven/sfgpetclinic/services/PetService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/heaven/sfgpetclinic/services/PetService.java
@@ -2,10 +2,6 @@ package com.heaven.sfgpetclinic.services;
 
 import com.heaven.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet,Long>{
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
