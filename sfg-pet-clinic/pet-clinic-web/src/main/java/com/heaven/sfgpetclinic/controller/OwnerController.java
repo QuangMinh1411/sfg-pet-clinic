e81b676750d417bf1b761e8e7fbceda5bf4be570
QuangMinh1411 <TomMinhQuan1411@gmail.com>
2022-07-11 15:39:41 +0700

    List all owners

--- sfg-pet-clinic/pet-clinic-web/src/main/java/com/heaven/sfgpetclinic/controller/OwnerController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/com/heaven/sfgpetclinic/controller/OwnerController.java
@@ -1,14 +1,20 @@
 package com.heaven.sfgpetclinic.controller;
 
+import com.heaven.sfgpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+    @Autowired
+    private OwnerService ownerService;
     @GetMapping({"","/index","/index.html"})
-    public String listAllOwners(){
+    public String listAllOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owner/index";
     }
 }
